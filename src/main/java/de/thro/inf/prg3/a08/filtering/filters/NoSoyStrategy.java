5bb376ec1aae09f17e49940cde53ec703de41c51
Peter Kurfer <peter.kurfer@fh-rosenheim.de>
2018-11-29 17:44:34 +0100

    Optimized imports

--- src/main/java/de/thro/inf/prg3/a08/filtering/filters/NoSoyStrategy.java
+++ src/main/java/de/thro/inf/prg3/a08/filtering/filters/NoSoyStrategy.java
@@ -1,8 +1,6 @@
 package de.thro.inf.prg3.a08.filtering.filters;
 
-import de.thro.inf.prg3.a08.filtering.FilterBase;
 import de.thro.inf.prg3.a08.filtering.NotesFilterBase;
-import de.thro.inf.prg3.a08.model.Meal;
 
 /**
  * @author Peter Kurfer
